5bd7540b0719608f474cdf78fd40d879a33e3df8
Jorge Bublitz <brjedi@gmail.com>
2022-08-11 17:59:01 -0300

    fix Initial Theme load from preferences

--- lib/theme_model.dart
+++ lib/theme_model.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
 import 'theme_preference.dart';
 
@@ -11,68 +13,11 @@ class ThemeModel extends ChangeNotifier {
   ThemeData get theme => _theme;
 
   ThemeModel() {
-    _team = '';
-    _preferences = ThemePreferences();
-    _icon = Icons.wb_sunny;
-    _theme = ThemeData.light();
     getPreferences();
   }
 
   set team(String value) {
     _team = value;
-    switch (_team) {
-      case 'Dark':
-        {
-          _icon = Icons.nightlight_round;
-          _theme = ThemeData.dark();
-          break;
-        }
-      case 'Bank':
-        {
-          _icon = Icons.account_balance_sharp;
-          _theme = ThemeData(
-            primarySwatch: Colors.pink,
-          );
-          break;
-        }
-      case 'Adb':
-        {
-          _icon = Icons.adb;
-          _theme = ThemeData(
-            primarySwatch: Colors.blue,
-          );
-          break;
-        }
-      case 'Air':
-        {
-          _icon = Icons.air;
-          _theme = ThemeData(
-            primarySwatch: Colors.red,
-          );
-          break;
-        }
-      case 'Bus':
-        {
-          _icon = Icons.bus_alert;
-          _theme = ThemeData(
-            primarySwatch: Colors.orange,
-          );
-          break;
-        }
-      case 'Corona':
-        {
-          _icon = Icons.coronavirus;
-          _theme = ThemeData(
-            primarySwatch: Colors.green,
-          );
-          break;
-        }
-      default:
-        {
-          _icon = Icons.wb_sunny;
-          _theme = ThemeData.light();
-        }
-    }
     _preferences.setTheme(value);
     notifyListeners();
   }
@@ -82,68 +27,102 @@ class ThemeModel extends ChangeNotifier {
       case 'Dark':
         {
           _team = 'Bank';
+          break;
+        }
+      case 'Bank':
+        {
+          _team = 'Adb';
+          break;
+        }
+      case 'Adb':
+        {
+          _team = 'Air';
+          break;
+        }
+      case 'Air':
+        {
+          _team = 'Bus';
+          break;
+        }
+      case 'Bus':
+        {
+          _team = 'Corona';
+          break;
+        }
+      case 'Corona':
+        {
+          _team = 'Light';
+          break;
+        }
+      default:
+        {
+          _team = 'Dark';
+        }
+    }
+    _preferences.setTheme(_team);
+    updateTeam();
+    notifyListeners();
+  }
+
+  void updateTeam() async {
+    switch (_team) {
+      case 'Dark':
+        {
+          _icon = Icons.nightlight_round;
+          _theme = ThemeData.dark();
+          break;
+        }
+      case 'Bank':
+        {
           _icon = Icons.account_balance_sharp;
           _theme = ThemeData(
             primarySwatch: Colors.pink,
           );
           break;
         }
-      case 'Bank':
+      case 'Adb':
         {
-          _team = 'Adb';
           _icon = Icons.adb;
           _theme = ThemeData(
             primarySwatch: Colors.blue,
           );
           break;
         }
-      case 'Adb':
+      case 'Air':
         {
-          _team = 'Air';
           _icon = Icons.air;
           _theme = ThemeData(
             primarySwatch: Colors.red,
           );
           break;
         }
-      case 'Air':
+      case 'Bus':
         {
-          _team = 'Bus';
           _icon = Icons.bus_alert;
           _theme = ThemeData(
             primarySwatch: Colors.orange,
           );
           break;
         }
-      case 'Bus':
+      case 'Corona':
         {
-          _team = 'Corona';
           _icon = Icons.coronavirus;
           _theme = ThemeData(
             primarySwatch: Colors.green,
           );
           break;
         }
-      case 'Corona':
-        {
-          _team = 'Light';
-          _icon = Icons.wb_sunny;
-          _theme = ThemeData.light();
-          break;
-        }
       default:
         {
-          _team = 'Dark';
-          _icon = Icons.nightlight_round;
-          _theme = ThemeData.dark();
+          _icon = Icons.wb_sunny;
+          _theme = ThemeData.light();
         }
     }
-    _preferences.setTheme(_team);
-    notifyListeners();
   }
 
   getPreferences() async {
     _team = await _preferences.getTheme();
+    updateTeam();
     notifyListeners();
   }
 }
